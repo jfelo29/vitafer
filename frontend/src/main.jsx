18587ff27360f600d6e452f5f5449309667fde78
¨felipe¨ <juanpipo03@gmail.com>
2025-09-17 19:41:28 -0500

    cambio de comint

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "../vendor/index.css";
-//import App from "./app.jsx";
+import App from "./app.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
