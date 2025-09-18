2aa0fbd24760eb9315f96e5ef6c29dca914edf55
¨felipe¨ <juanpipo03@gmail.com>
2025-09-17 19:57:01 -0500

    prueba de arreglo de rutas de html

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Main from "./componets/Main/Main.jsx";
 import Footer from "./componets/footer/Footer.jsx";
 
 
-import CurrentUserContext from "./contexts/currentUserContext.js";
+
 function App() {
  return (
   <div className="App">
